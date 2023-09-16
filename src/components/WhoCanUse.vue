f427c404108b164f3c32f1807cffe23dcd8c1b76
Zoha2400 <vasv29834@gmail.com>
2023-09-16 23:36:16 +0500

    Banner

--- src/components/WhoCanUse.vue
+++ src/components/WhoCanUse.vue
@@ -14,10 +14,33 @@
 
         </div>
 
-        <div class="ourOffers" id="offers">
+        <div class="ourOffers">
+            <p><span>ДЛЯ КОГО ПОДХДИТ</span> НАША УСЛУГА</p>
+        </div>
 
-            <p><span>ДЛЯ КОГО ПОДХДИТ</span>НАША УСЛУГА</p>
+        <div class="container-wrap banner-wrap" id="offers">
+                <div class="container banner">
+                    <div class="banner-item">
+                        <Icon icon="ic:baseline-percent" width="90"/>
+                        <p>Тем, кто хочет сэкономить деньги до 70%</p>
+                    </div>
+                    <div class="banner-item">
+                        <Icon icon="quill:calendar" width="90"/>
+                        <p>Тем, кто не хочет терять месяц на создание нового проекта</p>
+                    </div>
+                    <div class="banner-item">
+                        <Icon icon="tabler:checkup-list" width="90"/>
+                        <p>Тем, кто ищет проверенные решения</p>
+                    </div>
+                    <div class="banner-item">
+                        <Icon icon="game-icons:time-bomb" width="90"/>
+                        <p>Тем, кто ценит свое время и умеет считать деньги</p>
+                    </div>
+                </div>
+        </div>
 
+        <div class="ourOffers">
+            <p><span>НАША КОМПАНИЯ</span> В ЦИФРАХ</p>
         </div>
 
     </div>
