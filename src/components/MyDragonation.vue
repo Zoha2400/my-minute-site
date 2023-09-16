74c511d460d729ed7f18030e82a2c5a910b63093
Zoha2400 <vasv29834@gmail.com>
2023-09-16 22:36:50 +0500

    @iconofy

--- src/components/MyDragonation.vue
+++ src/components/MyDragonation.vue
@@ -1,3 +1,8 @@
+<script setup lang='ts'>
+
+
+</script>
+
 <template>
 
     <div class="draganation">
--- src/components/NavLinks.vue
+++ src/components/NavLinks.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
-
+    import { Icon } from '@iconify/vue';
 </script>
 
 <template>
     <div class="links">
         <h3>КОЛ-ЦЕНТР</h3>
-        <h1 class="marked">71 200 35 35</h1>
+        <h1 class="marked">
+            <Icon icon="ic:baseline-local-phone"/>
+            
+            71 200 35 35
+        </h1>
+
+
+        <div class="soc-link">
+            <a href="#">
+                <Icon icon="la:telegram" width="23"/>
+            </a>
+            <a href="#">
+                <Icon icon="lucide:instagram" width="23"/>
+            </a>
+            <a href="#">
+                <Icon icon="lucide:facebook" width="23"/>
+            </a>
+            <a href="#">
+                <Icon icon="lucide:youtube" width="23"/>
+            </a>
+        </div>
+    
     </div>
 </template>
 
