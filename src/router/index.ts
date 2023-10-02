fb605b6c416561fdf52686cedab5a30f4a2b07bd
Zoha2400 <vasv29834@gmail.com>
2023-10-02 20:31:23 +0500

    Router Complited

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import WhoCanUseVue from '@/components/WhoCanUse.vue';
 import MyProjectsVue from '@/components/MyProjects.vue';
 import NavBarProj from '@/components/NavBarProj.vue';
+import NavManu from '@/components/NavMenu.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {path: '/', components: {
       default: WhoCanUseVue,
+      navbar: NavManu,
     }},
     {path: '/projects', components: {
       default: MyProjectsVue,
