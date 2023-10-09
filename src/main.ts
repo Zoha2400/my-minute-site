3b92a89b85bcba9bb0cd3038494600b7817e3025
Zoha2400 <vasv29834@gmail.com>
2023-10-09 23:54:59 +0500

    Added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import store from './store/index'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(store)
 app.use(router)
 
 app.mount('#app')
