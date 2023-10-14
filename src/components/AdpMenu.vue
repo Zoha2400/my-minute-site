544127f28c5f1b8d342601a836d9155545dfc7c4
Zoha2400 <vasv29834@gmail.com>
2023-10-14 05:26:38 +0500

    card adp

--- src/components/AdpMenu.vue
+++ src/components/AdpMenu.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div :class="{'adpMenu nonHide': !$store.state.adpMenu,'adpMenu hideAdp': $store.state.adpMenu}">
+<div :class="{'adpMenu nonHide': !$store.state.adpMenu,'adpMenu hideAdp': $store.state.adpMenu}" @click="$store.commit('showAdpMenu')">
     <router-view name="navbar"></router-view>
 </div>
 
