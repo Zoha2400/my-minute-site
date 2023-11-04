c74999bb03d294111f7510ed8c6d5a210067f0a6
Zohir <vasv29834@gmail.com>
2023-11-04 10:54:25 +0500

    Added nav-reg-log

---
+++ src/components/RegLogBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Icon } from '@iconify/vue';
+
+</script>
+
+
+<template> 
+    <nav class="navbar-menu">
+        <RouterLink class="btn clickedNav" to="/">
+            <Icon icon="material-symbols:arrow-back" width="23"/>
+        </RouterLink>
+    </nav>
+
+    <RouterLink class="btn-in" to="/reg">
+        <p>Регистрация</p>
+    </RouterLink>
+    <RouterLink class="btn-in" to="/login">
+        <p>Вход</p>
+    </RouterLink>
+
+
+</template>
+
+
+
+<style>
+
+@import url('../assets/navbar.scss');
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import NavBarProj from '@/components/NavBarProj.vue';
 import NavManu from '@/components/NavMenu.vue';
 import RegPageVue from '@/components/RegPage.vue';
 import LogPageVue from '@/components/LogPage.vue';
+import RegLogBarVue from '@/components/RegLogBar.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,8 +18,14 @@ const router = createRouter({
       default: MyProjectsVue,
       navbar: NavBarProj,
     }},
-    {path: '/reg', component: RegPageVue},
-    {path: '/login', component: LogPageVue}
+    {path: '/reg', components: {
+      default: RegPageVue,
+      navbar: RegLogBarVue,
+    }},
+    {path: '/login', components: {
+      default: LogPageVue,
+      navbar: RegLogBarVue,
+    }}
   ],
   })
 
