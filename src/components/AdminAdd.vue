fc29e97e0513878ee40fab98683d70122de28fa8
Zohir <vasv29834@gmail.com>
2023-12-23 01:31:08 +0500

    Updated

--- src/components/AdminAdd.vue
+++ src/components/AdminAdd.vue
@@ -24,10 +24,18 @@
         </div>
         <div class="body-info">
           <p>Размер: <input type="text" v-model="data.size" /></p>
+          <button class="inpButton" @click="changeSize">Обновить</button>
+
           <p>Площадь: <input type="text" v-model="data.area" /></p>
+          <button class="inpButton" @click="changeArea">Обновить</button>
           <p>Cоток: <input type="text" v-model="data.acres" /></p>
+          <button class="inpButton" @click="changeAcres">Обновить</button>
+
           <p>Стиль: <input type="text" v-model="data.style" /></p>
+          <button class="inpButton" @click="changeStyle">Обновить</button>
+
           <p>Цена: <input type="text" v-model="data.cost" /></p>
+          <button class="inpButton" @click="changeCost">Обновить</button>
         </div>
 
         <div class="footer-info">
@@ -39,6 +47,7 @@
             placeholder="Информация"
             v-model="data.data"
           ></textarea>
+          <button class="inpButton" @click="changeData">Обновить</button>
         </div>
         <input
           type="file"
@@ -47,6 +56,8 @@
           name="mainphoto"
           id="mainphoto"
         />
+        <button class="inpButton" @click="changeArea">Обновить</button>
+
         <input
           type="file"
           @change="handleFileChangeOther"
@@ -56,8 +67,8 @@
           multiple
           accept="image/*"
         />
+        <button class="inpButton" @click="changeArea">Обновить</button>
 
-        <button class="inpButton" @click="add">Обновить</button>
         <button @click="$store.commit('closeInfoAdmin')">Закрыть</button>
       </div>
     </div>
@@ -120,19 +131,8 @@ function handleFileChangeOther(event: { target: any }) {
   }
 }
 
-function add() {
+function changeArea() {
   const formData = new FormData()
-
-  // Добавляем файлы
-  if (data.value.main_photo) {
-    formData.append('main_photo', data.value.main_photo)
-  }
-
-  if (data.value.photos.length > 0) {
-    data.value.photos.forEach((el) => {
-      formData.append('photos', el)
-    })
-  }
 
   formData.append('area', data.value.area)
   formData.append('pk', store.state.choosenAdmin.pk)
@@ -143,6 +143,111 @@ function add() {
   }
 
   fetch('http://localhost:3000/api/change/area', requestOptions)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log('Успешно отправлено:', responseData)
+    })
+    .catch((error) => {
+      console.error('Ошибка при отправке:', error)
+    })
+}
+
+function changeSize() {
+  const formData = new FormData()
+
+  formData.append('size', data.value.size)
+  formData.append('pk', store.state.choosenAdmin.pk)
+
+  const requestOptions = {
+    method: 'POST',
+    body: formData
+  }
+
+  fetch('http://localhost:3000/api/change/size', requestOptions)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log('Успешно отправлено:', responseData)
+    })
+    .catch((error) => {
+      console.error('Ошибка при отправке:', error)
+    })
+}
+
+function changeAcres() {
+  const formData = new FormData()
+
+  formData.append('acres', data.value.acres)
+  formData.append('pk', store.state.choosenAdmin.pk)
+
+  const requestOptions = {
+    method: 'POST',
+    body: formData
+  }
+
+  fetch('http://localhost:3000/api/change/acres', requestOptions)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log('Успешно отправлено:', responseData)
+    })
+    .catch((error) => {
+      console.error('Ошибка при отправке:', error)
+    })
+}
+
+function changeStyle() {
+  const formData = new FormData()
+
+  formData.append('style', data.value.style)
+  formData.append('pk', store.state.choosenAdmin.pk)
+
+  const requestOptions = {
+    method: 'POST',
+    body: formData
+  }
+
+  fetch('http://localhost:3000/api/change/style', requestOptions)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log('Успешно отправлено:', responseData)
+    })
+    .catch((error) => {
+      console.error('Ошибка при отправке:', error)
+    })
+}
+
+function changeCost() {
+  const formData = new FormData()
+
+  formData.append('cost', data.value.cost)
+  formData.append('pk', store.state.choosenAdmin.pk)
+
+  const requestOptions = {
+    method: 'POST',
+    body: formData
+  }
+
+  fetch('http://localhost:3000/api/change/cost', requestOptions)
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log('Успешно отправлено:', responseData)
+    })
+    .catch((error) => {
+      console.error('Ошибка при отправке:', error)
+    })
+}
+
+function changeData() {
+  const formData = new FormData()
+
+  formData.append('data', data.value.data)
+  formData.append('pk', store.state.choosenAdmin.pk)
+
+  const requestOptions = {
+    method: 'POST',
+    body: formData
+  }
+
+  fetch('http://localhost:3000/api/change/data', requestOptions)
     .then((response) => response.json())
     .then((responseData) => {
       console.log('Успешно отправлено:', responseData)
