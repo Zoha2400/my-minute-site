d78c14554c6c9fdba77dabe19646e0fc4a917454
Zohir <vasv29834@gmail.com>
2023-12-27 04:38:57 +0500

    link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-    import icon from '../img/icon.svg';
+import icon from '../img/icon.svg'
 </script>
 
 <template>
-    <nav class="navbar-wrap">
-        <div class="navbar">
-            <img :src="icon">
+  <nav class="navbar-wrap">
+    <div class="navbar">
+      <RouterLink to="/">
+        <img :src="icon" />
+      </RouterLink>
 
-            <router-view name="navbar"></router-view>
-        </div>
-    </nav>
+      <router-view name="navbar"></router-view>
+    </div>
+  </nav>
 </template>
 
-
 <style>
-
 @import url('../assets/navbar.scss');
-
 </style>
